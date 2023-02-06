dd26d586a9742002c0b9b74cab383f439ece159c
crcleavity@gmail.com <longc@o-7152a-03.studat.chalmers.se>
2023-02-06 11:37:48 +0100

    orginal version java codes are compiled running in StuDAT

---
+++ ExampleCaster.java
@@ -0,0 +1,49 @@
+
+import mcgui.*;
+
+/**
+ * Simple example of how to use the Multicaster interface.
+ *
+ * @author Andreas Larsson &lt;larandr@chalmers.se&gt;
+ */
+public class ExampleCaster extends Multicaster {
+
+    /**
+     * No initializations needed for this simple one
+     */
+    public void init() {
+        mcui.debug("The network has "+hosts+" hosts!");
+    }
+        
+    /**
+     * The GUI calls this module to multicast a message
+     */
+    public void cast(String messagetext) {
+        for(int i=0; i < hosts; i++) {
+            /* Sends to everyone except itself */
+            if(i != id) {
+                bcom.basicsend(i,new ExampleMessage(id, messagetext));
+            }
+        }
+        mcui.debug("Sent out: \""+messagetext+"\"");
+        mcui.deliver(id, messagetext, "from myself!");
+    }
+    
+    /**
+     * Receive a basic message
+     * @param message  The message received
+     */
+    public void basicreceive(int peer,Message message) {
+        mcui.deliver(peer, ((ExampleMessage)message).text);
+    }
+
+    /**
+     * Signals that a peer is down and has been down for a while to
+     * allow for messages taking different paths from this peer to
+     * arrive.
+     * @param peer	The dead peer
+     */
+    public void basicpeerdown(int peer) {
+        mcui.debug("Peer "+peer+" has been dead for a while now!");
+    }
+}
---
+++ ExampleMessage.java
@@ -0,0 +1,28 @@
+
+import mcgui.*;
+
+/**
+ * Message implementation for ExampleCaster.
+ *
+ * @author Andreas Larsson &lt;larandr@chalmers.se&gt;
+ */
+public class ExampleMessage extends Message {
+        
+    String text;
+        
+    public ExampleMessage(int sender,String text) {
+        super(sender);
+        this.text = text;
+    }
+    
+    /**
+     * Returns the text of the message only. The toString method can
+     * be implemented to show additional things useful for debugging
+     * purposes.
+     */
+    public String getText() {
+        return text;
+    }
+    
+    public static final long serialVersionUID = 0;
+}
